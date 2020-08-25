49ba9bcb224f492400a23e96299ea365e925c265
Aleksandar Velichkov <sashko98@gmail.com>
2020-08-25 20:24:46 +0300

    Started Greed is good kata

---
+++ JS/Greed is Good.js
@@ -0,0 +1,13 @@
+function score( dice ) {
+  let scoreboard = {};
+  dice.forEach(function (e) {
+    if (scoreboard.hasOwnProperty(e)) {
+      
+    }
+    
+  });
+}
+
+console.log(score( [2, 3, 4, 6, 2] ));
+console.log(score( [4, 4, 4, 3, 3] ));
+console.log(score( [2, 4, 4, 5, 4] ));
