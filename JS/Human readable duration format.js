25bcc81ccdf78a86a086fe081e388cc50fd90ce4
Aleksandar Velichkov <sashko98@gmail.com>
2020-08-25 22:37:24 +0300

    Started work on "Human readable duration format"

---
+++ JS/Human readable duration format.js
@@ -0,0 +1,23 @@
+function formatDuration (seconds) {
+  let time = {
+    seconds: seconds % 60,
+    minutes: Math.floor(seconds / 60) % 60,
+    hours: Math.floor(seconds / 60 / 60) % 24,
+    days: Math.floor(seconds / 60 / 60 / 24) % 365,
+    years: Math.floor(seconds / 60 / 60 / 24 / 365)
+  }
+
+  console.log(time);
+  let temp = Object.keys(time).map(e => [e, time[e]]).reduce(e => e[1] > 0);
+  console.log(temp);
+  return seconds;
+}
+
+console.log(formatDuration(1));
+console.log(formatDuration(62));
+console.log(formatDuration(120));
+console.log(formatDuration(3600));
+console.log(formatDuration(3662));
+console.log(formatDuration(86401));
+console.log(formatDuration(30879001));
+console.log(formatDuration(31536001));
