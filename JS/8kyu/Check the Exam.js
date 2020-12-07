0c0f94d382820bc95f40a2951446a3a0e412379c
Aleksandar Velichkov <sashko98@gmail.com>
2020-12-07 16:36:04 +0200

    solved check the exam

---
+++ JS/8kyu/Check the Exam.js
@@ -0,0 +1,10 @@
+function checkExam(arr1, arr2) {
+  let result = arr1.reduce((acc,e,i) => acc + ((arr1[i] === arr2[i]) ? 4 : (arr2[i].length) ? -1 : 0), 0);
+  return result < 0 ? 0 : result; 
+}
+
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]));
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]));
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]));
+console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]));
